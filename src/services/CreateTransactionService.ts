051aee35ff16d67bd121b98a14d8ea1693236000
JoaoVSouto <joaovitorss99@hotmail.com>
2020-11-07 14:42:44 -0300

    Creates service for creating a new transaction

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,61 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository, getRepository } from 'typeorm';
 
+import AppError from '../errors/AppError';
+
+import Category from '../models/Category';
 import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category: categoryTitle,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    if (type === 'outcome') {
+      const { total } = await transactionsRepository.getBalance();
+
+      if (total < value) {
+        throw new AppError(
+          "You don't have enough balance to buy this product.",
+        );
+      }
+    }
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    const categoriesRepository = getRepository(Category);
+
+    const category = await categoriesRepository.findOne({
+      where: { title: categoryTitle },
+    });
+
+    if (!category) {
+      const newCategory = categoriesRepository.create({ title: categoryTitle });
+      await categoriesRepository.save(newCategory);
+
+      transaction.category = newCategory;
+    } else {
+      transaction.category = category;
+    }
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
