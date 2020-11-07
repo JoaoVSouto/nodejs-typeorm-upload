203c9445e8b4a12ef6c55b4f17fe1dea76a7f26b
JoaoVSouto <joaovitorss99@hotmail.com>
2020-11-07 14:11:11 -0300

    Creates getBalance method on transactions repository

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,31 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+
+    const income = transactions
+      .filter(transaction => transaction.type === 'income')
+      .reduce(
+        (totalIncome, currentTransaction) =>
+          totalIncome + currentTransaction.value,
+        0,
+      );
+
+    const outcome = transactions
+      .filter(transaction => transaction.type === 'outcome')
+      .reduce(
+        (totalOutcome, currentTransaction) =>
+          totalOutcome + currentTransaction.value,
+        0,
+      );
+
+    const total = income - outcome;
+
+    return {
+      income,
+      outcome,
+      total,
+    };
   }
 }
 
